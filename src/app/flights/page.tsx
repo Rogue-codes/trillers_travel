7838162876f1ea14f61032c3d8db28a2ae4e5594
derabola <adu.deraj@gmail.com>
2024-02-15 19:40:40 +0000

    commit

--- src/app/flights/page.tsx
+++ src/app/flights/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '@/components/NavTopComponent/NavTopComponent';
 
 function Flights() {
+  const [activeTab,seActiveTab] = useState(0)
   return (
     <div className='flex min-h-screen ml-[250px] bg-[#e1eceb] rounded-tr-xl rounded-br-xl pt-10 flex-col items-start justify-start px-10'>
       <Navbar />
